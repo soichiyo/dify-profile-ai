4bf739869677217f08cc44744f4339c9e92709aa
soichiyo <soichiro0111.dev@gmail.com>
2025-08-27 09:59:30 +0900

    delete multi langs

--- i18n/i18next-serverside-config.ts
+++ i18n/i18next-serverside-config.ts
@@ -1,26 +1,24 @@
 import { createInstance } from 'i18next'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next/initReactI18next'
-import type { Locale } from '.'
 
-// https://locize.com/blog/next-13-app-dir-i18n/
-const initI18next = async (lng: Locale, ns: string) => {
+const initI18next = async (ns: string) => {
   const i18nInstance = createInstance()
   await i18nInstance
     .use(initReactI18next)
-    .use(resourcesToBackend((language: string, namespace: string) => import(`./lang/${namespace}.${language}.ts`)))
+    .use(resourcesToBackend((language: string, namespace: string) => import(`./lang/${namespace}.ja.ts`)))
     .init({
-      lng: lng === 'zh-Hans' ? 'zh' : lng,
+      lng: 'ja',
       ns,
-      fallbackLng: 'en',
+      fallbackLng: 'ja',
     })
   return i18nInstance
 }
 
-export async function useTranslation(lng: Locale, ns = '', options: Record<string, any> = {}) {
-  const i18nextInstance = await initI18next(lng, ns)
+export async function useTranslation(ns = '', options: Record<string, any> = {}) {
+  const i18nextInstance = await initI18next(ns)
   return {
-    t: i18nextInstance.getFixedT(lng, ns, options.keyPrefix),
+    t: i18nextInstance.getFixedT('ja', ns, options.keyPrefix),
     i18n: i18nextInstance,
   }
 }
